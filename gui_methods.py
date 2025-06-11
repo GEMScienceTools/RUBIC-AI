0284e2d427ea5f00f5b80e5697c464507c4f6fbe
Daniel Gómez <85584005+dangomezm@users.noreply.github.com>
2025-06-11 09:41:09 -0500

    Add files via upload

--- gui_methods.py
+++ gui_methods.py
@@ -1659,7 +1659,7 @@ class GUIMethods:
                     self.setComboBoxByData(self.ui.bck_pos_cb_3 , self.data_ai.iloc[self.click_count * 3 + 2 , 10])
         
                 # Epoch of construction
-                if self.data_ai.iloc[self.click_count * 3 + 2 + 1 , 11] is None :
+                if self.data_ai.iloc[self.click_count * 3 + 2 , 11] is None :
                     self.ui.epc_const_cb_3.setCurrentText("Select Epoch of Construction")
                 elif pd.isna(self.data_ai.iloc[self.click_count * 3 + 2 +1 , 11]) == True:
                     self.ui.epc_const_cb_3.setCurrentText("Select Epoch of Construction")
